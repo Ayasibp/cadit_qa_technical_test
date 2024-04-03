60deca33a79f2f43e00ceee3ea454e4ca1d6eb5a
ayasi.priyoda <ayasi.priyoda@shopee.com>
2024-04-03 13:28:36 +0700

    fix : fix movie model

--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -2,6 +2,10 @@ import db from "../models/index.js"
 
 const { Movie } = db.db
 
-const findAll = async () => Movie.findAll({})
+const findAll = async () =>
+  Movie.findAll({
+    attributes: ["movies", "one_line"],
+    limit: 100,
+  })
 
 export { findAll }
